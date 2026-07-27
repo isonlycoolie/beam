03cedd1fe0f543c5f448053f334ac70508933e26
isonlycoolie <denisimbuya8@gmail.com>
2026-07-27 21:54:21 +0300

    Export structured Beam error modules for Beam

---
+++ packages/core/src/errors.ts
@@ -0,0 +1,5 @@
+export * from "./errors/base.js";
+export * from "./errors/figma.js";
+export * from "./errors/filesystem-compare.js";
+export * from "./errors/input-auth.js";
+export * from "./errors/payload.js";
